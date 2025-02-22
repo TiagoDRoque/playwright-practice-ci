051ddd778652f51fe772843a599d25b4ecb546a5
TiagoDRoque <tiagoprogramming7@gmail.com>
2025-02-22 17:36:28 +0000

    fix

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 test('TC-001: Verify that a "Standard_user" can access the homepage', async ({ page }) => {
@@ -9,7 +8,7 @@ test('TC-001: Verify that a "Standard_user" can access the homepage', async ({ p
     await page.fill('#user-name', process.env.STANDARD_USER as string);
     await page.fill('#password', process.env.PASSWORD as string);
     await page.click('#login-button');
+    await page.waitForURL('**/inventory.html', { timeout: 10000 });
     const url = page.url();
-    expect(url).toBe('https://www.saucedemo.com/inventory.html');
+    expect(url).toBe('***/inventory.html')
 });
-
